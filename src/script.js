a2e41b8f57ea1fa135052da0fb9d3ec183c7e127
RafaelSd0 <player08901@gmail.com>
2024-09-19 19:53:49 -0300

    commit

---
+++ src/script.js
@@ -0,0 +1,43 @@
+function calcularIMC(event){
+    event.preventDefault();
+
+    var peso = document.getElementById('peso').value;
+    var altura = document.getElementById('altura').value;
+
+    let IMC = peso / (altura * altura)
+
+    if (IMC < 18.5) {
+        document.getElementById('saida').innerText = "O seu IMC é: " + IMC.toFixed(2) + " está abaixo do peso";
+    } if(IMC >= 18.5 && IMC < 25) {
+        document.getElementById('saida').innerText = "O seu IMC é: " + IMC.toFixed(2) + " está com o peso ideal";
+    } if(IMC >= 25 && IMC < 30) {
+        document.getElementById('saida').innerText = "O seu IMC é: " + IMC.toFixed(2) + " está com sobrepeso";
+    } if(IMC >= 30 && IMC < 35) {
+        document.getElementById('saida').innerText = "O seu IMC é: " + IMC.toFixed(2) + " está com obesidade grau 1";
+    } if(IMC >= 35 && IMC < 40) {
+        document.getElementById('saida').innerText = "O seu IMC é: " + IMC.toFixed(2) + " está com obesidade grau 2";
+    } if(IMC >= 40) {
+        document.getElementById('saida').innerText = "O seu IMC é: " + IMC.toFixed(2) + " está com obesidade grau 3";
+    }
+
+
+}
+
+let menuExpansivo = false;
+
+function expandir() {
+    const menuMobile = document.getElementById('menuMobile');
+    if (!menuExpansivo) {
+        menuMobile.style.height = '110px'; // Expande o menu
+        menuExpansivo = true;
+    } else {
+        menuMobile.style.height = '30px'; // Recolhe o menu
+        menuExpansivo = false;
+    }
+}
+
+function recolherMenu() {
+    const menuMobile = document.getElementById('menuMobile');
+    menuMobile.style.height = '30px'; // Recolhe o menu
+    menuExpansivo = false; // Reseta o estado do menu
+}
